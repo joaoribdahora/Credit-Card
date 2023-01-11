5dd065e2c8711712beeac6104aeaf6d6ad679662
João Victor Ribeiro <joao.leiterib19@gmail.com>
2023-01-11 18:16:39 -0300

    Organização da estrutura do JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
+// Arrays necessários para fazer a validação do formulário
 let cardNumb = ['0','1','2','3','4','5','6','7','8','9'];
 let dateMonth = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-let dateYear = '';
 let alfabet = [' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let cvcNumber = '';
-let cardName = '';
-let arrayCardName = [];
-let controlName = 0;
 let numbers = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09'];
+
+// Arrays para as informações dadas pelo usuário
+let arrayCardName = [];
 let arrayCardNumber = [];
 
+// Evento de envio do formulário
 document.querySelector('button').addEventListener('click', send);
 
 //Essa função irá enviar as informações do cartão.
@@ -16,106 +16,106 @@ document.querySelector('button').addEventListener('click', send);
 
 function send(){
 
-// controle da informação do mês
+    //Verificação de nome
 
-    let controleMes = false;
+        let controlName = 0;
+        let cardName = document.querySelector('.cardName .importantInfo').value;
+        arrayCardName = cardName.toLocaleUpperCase().split('');
 
-    for(let i = 0; i<dateMonth.length; i++){
-        if(document.querySelector('.month .inpDate').value === dateMonth[i]){
-            controleMes = true;
-           
-        }
-    }
-    if(controleMes){
-        //Permitir o envio do formulário
-    }else{
-        //Bloquear o envio do formulário
-    }
-
-// controle da informação do CVC
-
-    cvcNumber = document.querySelector('.inpCVC').value;
-    if(cvcNumber.length !== 3){
-        //Bloquear o envio do formulário
-    }else{
-        //Permitir o envio do formulário
-    }
-
-//Verificação de nome correto
-    controlName = 0;
-    cardName = document.querySelector('.cardName .importantInfo').value;
-    arrayCardName = cardName.toLocaleUpperCase().split('');
-
-    for(let i = 0; i<arrayCardName.length; i++){
-        for(let j = 0; j<alfabet.length; j++){
-            if(arrayCardName[i] === alfabet[j]){
-                console.log(`${arrayCardName[i]} é igual ${alfabet[j]}`);
-                controlName++;
-            }
-        }
-    }
-
-    if(controlName === arrayCardName.length){
-        //Permitir o envio do formulário
-    }else{ 
-        //Bloquear o envio do formulário
-    }
-
-
-// Controle de informação do ano 
-
-    let inputYear = document.querySelector('.year .inpDate').value;
-    let controlYear = false;
-
-    
-    if(parseInt(inputYear) < 10 && parseInt(inputYear)>=0){
-        for(let i = 0; i<numbers.length; i++){
-            if(inputYear === numbers[i]){
-                controlYear = true;
-            }
-        }
-    }else{
-        if(parseInt(inputYear) >=10 && parseInt(inputYear)<=99){
-            controlYear = true;
-        }
-    }
-
-    if(controlYear){
-        //Permitir o envio do formulário
-    }else{
-        //Bloquear o envio do formulário
-    }
-
-
-//Controle do número do cartão
-
-    let controlCardNumber = false;
-    let sumControl = 0;
-    let inputCardNumber = document.querySelector('.number .importantInfo').value;
-    
-    arrayCardNumber = inputCardNumber.split('');
-    console.log(arrayCardNumber);
-
-    if(inputCardNumber.length === 16){
-        controlCardNumber = true;
-        console.log('quantidade de números correta');
-
-        for(let i = 0; i< arrayCardNumber.length; i++){
-            for(let j = 0; j< cardNumb.length; j++){
-                if(arrayCardNumber[i] === cardNumb[j]){
-                    sumControl++;
+        for(let i = 0; i<arrayCardName.length; i++){
+            for(let j = 0; j<alfabet.length; j++){
+                if(arrayCardName[i] === alfabet[j]){
+                    controlName++;
                 }
             }
         }
 
-        if(sumControl === 16){
+        if(controlName === arrayCardName.length){
+            //Permitir o envio do formulário
+        }else{ 
+            //Bloquear o envio do formulário
+        }
+
+
+    //Verificação do número do cartão
+
+        let controlCardNumber = false;
+        let sumControl = 0;
+        let inputCardNumber = document.querySelector('.number .importantInfo').value;
+        
+        arrayCardNumber = inputCardNumber.split('');
+
+        if(inputCardNumber.length === 16){
+            controlCardNumber = true;
+            console.log('quantidade de números correta');
+
+            for(let i = 0; i< arrayCardNumber.length; i++){
+                for(let j = 0; j< cardNumb.length; j++){
+                    if(arrayCardNumber[i] === cardNumb[j]){
+                        sumControl++;
+                    }
+                }
+            }
+
+            if(sumControl === 16){
+                //Permitir o envio do formulário
+            }else{
+                //Bloquear o envio do formulário
+            }
+
+        }else{
+            //Bloquear o envio do formulário
+        }
+
+
+    //Verificação da informação do mês
+
+        let controleMes = false;
+
+        for(let i = 0; i<dateMonth.length; i++){
+            if(document.querySelector('.month .inpDate').value === dateMonth[i]){
+                controleMes = true;
+            
+            }
+        }
+        if(controleMes){
             //Permitir o envio do formulário
         }else{
             //Bloquear o envio do formulário
         }
 
-    }else{
-        //Bloquear o envio do formulário
-    }
 
+    // Controle de informação do ano 
+
+        let inputYear = document.querySelector('.year .inpDate').value;
+        let controlYear = false;
+
+        
+        if(parseInt(inputYear) < 10 && parseInt(inputYear)>=0){
+            for(let i = 0; i<numbers.length; i++){
+                if(inputYear === numbers[i]){
+                    controlYear = true;
+                }
+            }
+        }else{
+            if(parseInt(inputYear) >=10 && parseInt(inputYear)<=99){
+                controlYear = true;
+            }
+        }
+
+        if(controlYear){
+            //Permitir o envio do formulário
+        }else{
+            //Bloquear o envio do formulário
+        }
+
+
+    // controle da informação do CVC
+
+        let cvcNumber = document.querySelector('.inpCVC').value;
+            if(cvcNumber.length !== 3){
+                //Bloquear o envio do formulário
+            }else{
+                //Permitir o envio do formulário
+            }
 }
